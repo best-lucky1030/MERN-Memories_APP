2a7320406ff7f0445fc8de77acd31f3bc7576a49
best-lucky <bestlucky1030@gmail.com>
2021-08-30 16:08:47 +0300

    add router and update controller in server part

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,35 +5,39 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async(req, res) => {
+export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
+
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({message: error.message});
+        res.status(404).json({ message: error.message });
     }
 }
 
-export const getPost = async(req, res) => {
-    const {id} = req.params;
+export const getPost = async (req, res) => {
+    const { id } = req.params;
+
     try {
-        const post = await PostMessage.findById();
+        const post = await PostMessage.findById(id);
+
         res.status(200).json(post);
-    } catch(error) {
-        res.status(404).json({message: error.message});
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
-export const createPost = async(req, res) => {
+export const createPost = async (req, res) => {
     const { title, message, selectedFile, creator, tags } = req.body;
 
-    const newPostMessage = new postMessage({title, message, selectedFile, creator, tag});
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
     try {
         await newPostMessage.save();
 
-        res.status(201).json(newPostMessage);
+        res.status(201).json(newPostMessage );
     } catch (error) {
-        res.status(409).json({message: error.message});
+        res.status(409).json({ message: error.message });
     }
 }
 
@@ -71,5 +75,6 @@ export const likePost = async (req, res) => {
 
     res.json(updatedPost);
 }
+
 
 export default router;
--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT || 5000;
